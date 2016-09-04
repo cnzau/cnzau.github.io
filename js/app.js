5820aa21a3421744d4977730cbc3fb96a48a93d8
cnzau <clement.nzau@gmail.com>
2016-09-04 10:28:17 +0300

    added autoexpand to contact message text area

--- js/app.js
+++ js/app.js
@@ -171,3 +171,20 @@ jQuery(document).ready(function($) {
         $newWord.removeClass('is-hidden').addClass('is-visible');
     }
 });
+
+// textarea auto-expand
+$(document)
+		.one('focus.textarea', '.autoExpand', function(){
+			var savedValue = this.value;
+			this.value = '';
+			this.baseScrollHeight = this.scrollHeight;
+			this.value = savedValue;
+		})
+		.on('input.textarea', '.autoExpand', function(){
+			var minRows = this.getAttribute('data-min-rows')|0,
+				 rows;
+			this.rows = minRows;
+        console.log(this.scrollHeight , this.baseScrollHeight);
+			rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
+			this.rows = minRows + rows;
+		});
