d13d6a3fe02f805630ca863ad85262eb2f168810
cnzau <clement.nzau@gmail.com>
2016-09-11 14:59:08 +0300

    changed the text auto expand script

--- js/app.js
+++ js/app.js
@@ -174,17 +174,17 @@ jQuery(document).ready(function($) {
 
 // textarea auto-expand
 $(document)
-		.one('focus.textarea', '.autoExpand', function(){
-			var savedValue = this.value;
-			this.value = '';
-			this.baseScrollHeight = this.scrollHeight;
-			this.value = savedValue;
-		})
-		.on('input.textarea', '.autoExpand', function(){
-			var minRows = this.getAttribute('data-min-rows')|0,
-				 rows;
-			this.rows = minRows;
-        console.log(this.scrollHeight , this.baseScrollHeight);
-			rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
-			this.rows = minRows + rows;
-		});
+    .on('focus.textarea', '.autoExpand', function() {
+        var savedValue = this.value;
+        this.value = '';
+        this.baseScrollHeight = this.scrollHeight;
+        this.value = savedValue;
+    })
+    .on('input.textarea', '.autoExpand', function() {
+        var minRows = this.getAttribute('data-min-rows') | 0,
+            rows;
+        this.rows = minRows;
+        console.log(this.scrollHeight, this.baseScrollHeight);
+        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 29.25);
+        this.rows = minRows + rows;
+    });
