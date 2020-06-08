2702a2ad0b3cf573231b62b1fd4af7b473314d82
cnzau <clement.nzau@gmail.com>
2020-06-08 16:56:53 +0300

    Home slideshow

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import './App.css';
+import Default from './layouts/Default';
+import './App.scss';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>Clement Nzau</header>
-    </div>
+    <>
+      <div className='App'>
+        <Default />
+        <div
+          style={{
+            backgroundColor: 'rgba(0,0,0,0.6)',
+            height: '100vh',
+            width: '100vw',
+            position: 'absolute',
+            left: 0,
+            top: 0,
+            zIndex: 0,
+          }}
+        >
+          <header>
+            <div className='brand'>
+              clement <br />
+              nzau <br />
+              timothy
+            </div>
+          </header>
+          <div>
+            <p>Hi,</p>
+            <p>
+              I am a<b className='is-visible'> Sofware Developer</b>
+              <b> Front-End Developer</b>
+              <b> Cloud Enginer</b>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/layouts/Default.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Default.scss';
+
+const Default = () => {
+  return (
+    <>
+      <ul class='cb-slideshow'>
+        <li>
+          <span></span>
+        </li>
+        <li>
+          <span></span>
+        </li>
+        <li>
+          <span></span>
+        </li>
+        <li>
+          <span></span>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Default;
